79302f002858e2ee1be0b7ebe6c9accadbd0b69d
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-13 15:42:37 +0100

    add models for the game

--- back_end/pong_game/pong_app/models.py
+++ back_end/pong_game/pong_app/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
-Class	PongBall(models.Model):
-	x = models.IntegerField()
-	y = models.IntegerField()
+Class	Player(models.Model):
+	firstName = models.CharField(max_length=20);
+	name = models.CharField(max_length=20);
+	idName = models.CharField(max_length=20);
+	# TODO : img = models.ImageField(upload_to='images/')
 
-Class	PongPaddle(models.Model):
-	x = models.IntegerField()
-	y = models.IntegerField()
+Class	Game(models.Model):
+	date = models.DateField();
+	hour = models.TimeField();
+	duration = models.IntegerField();
+	# TODO (pas utile puisqu'on a players.size() je pense) : nbRealPlayers = models.IntegerField(min=1);
+	players = models.ManyToManyField(Player);
+
+Class	Score(models.Model):
+	players = models.ForeignKey(Player, on_delete=models.CASCADE);
+	game = models.ForeignKey(Game, on_delete=models.CASCADE);
+	points = models.IntegerField();
