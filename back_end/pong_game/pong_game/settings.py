876432f5adcb18d94006aadcbcf47637b8acd32f
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-13 16:33:54 +0100

    add app to settings

--- back_end/pong_game/pong_game/settings.py
+++ back_end/pong_game/pong_game/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pong_app',
 ]
 
 MIDDLEWARE = [
