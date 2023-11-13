d00c9866290dcc4c53894a18ca8e4fc1af102905
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-13 16:50:24 +0100

    add first paddle

--- back_end/pong_game/pong_app/models.py
+++ back_end/pong_game/pong_app/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 
-Class	Player(models.Model):
+class	Player(models.Model):
 	firstName = models.CharField(max_length=20);
 	name = models.CharField(max_length=20);
 	idName = models.CharField(max_length=20);
 	# TODO : img = models.ImageField(upload_to='images/')
 
-Class	Game(models.Model):
+class	Game(models.Model):
 	date = models.DateField();
 	hour = models.TimeField();
 	duration = models.IntegerField();
 	# TODO (pas utile puisqu'on a players.size() je pense) : nbRealPlayers = models.IntegerField(min=1);
 	players = models.ManyToManyField(Player);
 
-Class	Score(models.Model):
-	players = models.ForeignKey(Player, on_delete=models.CASCADE);
-	game = models.ForeignKey(Game, on_delete=models.CASCADE);
-	points = models.IntegerField();
+class	Score(models.Model):
+	players = models.ForeignKey(Player, on_delete=models.CASCADE)
+	game = models.ForeignKey(Game, on_delete=models.CASCADE)
+	points = models.IntegerField()
+
+class	PongGameState(models.Model):
+    paddle_position = models.IntegerField(default=0)
--- back_end/pong_game/pong_app/views.py
+++ back_end/pong_game/pong_app/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.db import connection
+from pong_app.models import PongGameState
 
 def homePage(request):
     return render(request, 'homePage.html')
 
 def pongGame(request):
-    return render(request, 'pong_elements/pong_game.html')
+    game_state, created = PongGameState.objects.get_or_create(pk=1)
+
+    # Traitez les événements ou la logique du jeu ici
+    # Pour cet exemple, nous utilisons simplement la position de la raquette
+    if request.method == 'POST':
+        if 'up' in request.POST and game_state.paddle_position > 0:
+            game_state.paddle_position -= 10
+        elif 'down' in request.POST and game_state.paddle_position < 500:
+            game_state.paddle_position += 10
+
+        # Enregistrez la nouvelle position de la raquette dans la base de données
+        game_state.save()
+
+    return render(request, 'pong_elements/pong_game.html', {'game_state': game_state})
 
 def testDBConnection(request):
     try:
