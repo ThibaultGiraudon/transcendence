b48a4ea4b56f5752781a37b20c7df843731d4e50
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-13 16:00:03 +0100

    change url name to test db

--- back_end/pong_game/pong_app/urls.py
+++ back_end/pong_game/pong_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.homePage, name='homePage'),   	
     path('pong/', views.pongGame, name='pong'),
-    path('test_connection/', views.test_connection, name='test_connection'),
+    path('test/', views.testDBConnection, name='testDBConnection'),
 ]
