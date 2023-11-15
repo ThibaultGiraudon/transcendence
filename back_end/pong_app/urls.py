0b9b2e4bf78c88dddf7a48390ae638babdb1adc2
LeonPupier <leonpupier@gmail.com>
2023-11-15 12:59:29 +0100

    Add the login page

--- back_end/pong_app/urls.py
+++ back_end/pong_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homePage, name='homePage'),   	
+    path('', views.homePage, name='homePage'),
+    path('login/', views.login, name='login'),
     path('pong/', views.pongGame, name='pong'),
     path('test/', views.testDBConnection, name='testDBConnection'),
 ]
