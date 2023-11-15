3645ad885d1cde85083dff19adccf3ffcdd658af
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-15 15:56:38 +0100

    fix routing consumers and asgi for wss

--- django/pong_game/consumers.py
+++ django/pong_game/consumers.py
@@ -1,7 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-class SimpleConsumer(AsyncWebsocketConsumer):
+class PongConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
 
--- django/pong_game/routing.py
+++ django/pong_game/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/some_path/$', consumers.SimpleConsumer.as_asgi()),
+    re_path(r'ws/some_path/$', consumers.PongConsumer.as_asgi()),
 ]
