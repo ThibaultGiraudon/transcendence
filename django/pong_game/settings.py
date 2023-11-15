086db05dfe8dfcab23b1be96c3c3ef8bfb44f7ae
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-15 13:59:47 +0100

    change file for wss

--- django/pong_game/settings.py
+++ django/pong_game/settings.py
@@ -42,6 +42,12 @@ INSTALLED_APPS = [
 
 ASGI_APPLICATION = 'pong_game.asgi.application'
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    },
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
