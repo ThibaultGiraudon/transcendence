718e278526bd1337c78721567f5c186fbff4908e
Thibault Giraudon <tgiraudo@z3r7p5.42lyon.fr>
2023-11-15 16:24:03 +0100

    pong game working

--- back_end/pong_app/static/pong_app/js/main.js
+++ back_end/pong_app/static/pong_app/js/main.js
@@ -1,63 +1,153 @@
+const PLAYER_WIDTH = 20
+const PLAYER_HEIGHT = 150
+
 const canvas = document.getElementById("pongCanvas");
 const ctx = canvas.getContext("2d");
+var game;
 const startPaddle = canvas.height / 2 - 75
 
-const ball = {
-    x: canvas.width / 2,
-    y: canvas.height / 2
-};
 
-const velocity = 3;
-const startingAngle = 70;
-const rad = 20;
-let moveX = Math.cos(Math.PI / 180 * startingAngle) * velocity;
-let moveY = Math.sin(Math.PI / 180 * startingAngle) * velocity;
+function draw() {
+    drawBackground()
+    drawScore()
+    drawBall()
+    darwPaddles()
+}
 
-const drawMe = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (ball.x > canvas.width - rad || ball.x < rad) 
-        moveX = -moveX;
-    if (ball.y > canvas.height - rad || ball.y < rad)
-        moveY = -moveY; ball.x += moveX;
-    ball.y += moveY; ctx.beginPath();
-    ctx.fillStyle = 'green';
-    ctx.arc(ball.x, ball.y, rad, 0, Math.PI * 2);
-    ctx.fill();
+function drawScore() {
+    ctx.fillStyle = 'white'
+    ctx.font = "50px serif"
+    ctx.fillText(game.computer.score, 50, 55)
+    ctx.fillText(game.player.score, canvas.width - 75, 55)
+}
+
+function darwPaddles() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(5, game.player.y , PLAYER_WIDTH, PLAYER_HEIGHT);
+    ctx.fillRect(canvas.width - PLAYER_WIDTH - 5, game.computer.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+}
+
+function drawBackground() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.strokeStyle = 'white';
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, 0);
+    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.stroke();
     ctx.closePath();
 }
 
-// function darwPaddle() {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     ctx.fillStyle = "black";
-//     ctx.fillRect(0, 0, canvas.width, canvas.height);
-//     ctx.beginPath()
-//     ctx.fillStyle = "purple"
-//     ctx.fill()
-//     ctx.fillRect(5, startPaddle + deltaY, 20, 150);
-//     ctx.closePath();
-// }
+function drawBall() {
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.arc(game.ball.x, game.ball.y, game.ball.r, 0, Math.PI * 2, false);
+    ctx.fill();
+}
 
-// document.addEventListener('keydown', move, false);
+function play() {
+    draw();
+    
+    // computerMove();
+    ballMove();
+    requestAnimationFrame(play);
+}
 
-// var deltaY = 0
-setInterval(drawMe, 10);
-// darwPaddle()
+document.addEventListener('keydown', playerOneMove, false);
+document.addEventListener('keydown', playerTwoMove, false);
 
-// function move(e) {
-//     switch(e.key) {
-//         case 'w':
-//             if ((startPaddle + deltaY - 10) > 0)
-//             deltaY -= 10
-//         break;
-//         case 's':
-//             if ((startPaddle + deltaY + 10) < canvas.height - 150)
-//                 deltaY += 10
-//             break;
-//     }
-//     e.preventDefault();
 
-//     darwPaddle()
-// }
+function playerOneMove(e) {
+    switch(e.key) {
+        case 'w':
+            if ((game.player.y - 10) > 0)
+            game.player.y -= 10
+        break;
+        case 's':
+            if ((game.player.y + 10) < canvas.height - 150)
+                game.player.y += 10
+            break;
+    }
+    e.preventDefault();
+}
+
+function playerTwoMove(e) {
+    switch(e.key) {
+        case 'ArrowUp':
+            if ((game.computer.y - 10) > 0)
+            game.computer.y -= 10
+        break;
+        case 'ArrowDown':
+            if ((game.computer.y + 10) < canvas.height - 150)
+                game.computer.y += 10
+            break;
+    }
+    e.preventDefault();
+}
+
+function computerMove() {
+        game.computer.y += game.ball.speed.y * 0.85;
+}
+
+function ballMove() {
+    if (game.ball.y + game.ball.r > canvas.height || game.ball.y - game.ball.r < 0) {
+        game.ball.speed.y *= -1;
+    }
+
+    if (game.ball.x + game.ball.r > canvas.width - PLAYER_WIDTH - 5) {
+        collide(game.computer);
+    } else if (game.ball.x - game.ball.r < PLAYER_WIDTH + 5) {
+        collide(game.player);
+    }
+
+    game.ball.x += game.ball.speed.x;
+    game.ball.y += game.ball.speed.y;
+}
+
+function collide(player) {
+    // The player does not hit the ball
+    if (game.ball.y < player.y || game.ball.y > player.y + PLAYER_HEIGHT) {
+        // Set ball and players to the center
+        game.ball.x = canvas.width / 2;
+        game.ball.y = canvas.height / 2;
+        game.computer.y = canvas.height / 2 - PLAYER_HEIGHT / 2;
+        player.score++
+        // Reset speed
+        if (game.ball.speed.x > 0)
+            game.ball.speed.x = 2;
+        else 
+            game.ball.speed.x = -2
+    } else {
+        // Increase speed and change direction
+        game.ball.speed.x *= -1.2;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    game = {
+        player: {
+            y: canvas.height / 2 - PLAYER_HEIGHT / 2,
+            score: 0
+        },
+        computer: {
+            y: canvas.height / 2 - PLAYER_HEIGHT / 2,
+            score: 0
+        },
+        ball: {
+            x: canvas.width / 2,
+            y: canvas.height / 2,
+            r: 10,
+            speed: {
+                x: -2,
+                y: 2
+            }
+        }
+    }
+
+    draw();
+    play()
+})
 
 
 
