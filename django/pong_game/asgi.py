755ad8c1d5957f2e97221505dcb4190fb1f7c70a
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-15 13:59:47 +0100

    change file for wss

--- django/pong_game/asgi.py
+++ django/pong_game/asgi.py
@@ -11,15 +11,11 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import pong_game.routing
+from pong_game.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pong_game.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            pong_game.routing.websocket_urlpatterns
-        )
-    ),
+  'https': get_asgi_application(),
+  'websocket': AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
 })
