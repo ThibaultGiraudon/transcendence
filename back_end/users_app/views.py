d96630e69598031e35cf7583e634a545514aedbd
Thibault Giraudon <tibo@macbook-air-de-thibault.home>
2023-11-16 21:17:19 +0100

    add super user but dont work well

--- back_end/users_app/views.py
+++ back_end/users_app/views.py
@@ -18,7 +18,6 @@ def sign_in(request):
             if user:
                 login(request, user)
                 messages.success(request,f'Hi {username.title()}, welcome back!')
-                return redirect('posts')
         
         # form is not valid or user is not authenticated
         messages.error(request,f'Invalid username or password')
