49c2d364cfb195c9aa9e8198c49659997ccc041c
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-16 15:24:41 +0100

    add new version of websocket without all routing files

--- django/pong_game/asgi.py
+++ django/pong_game/asgi.py
@@ -7,19 +7,27 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from pong_game.routing import websocket_urlpatterns
+import  os
+
+from    django.core.asgi import get_asgi_application
+from    channels.routing import ProtocolTypeRouter, URLRouter
+from    channels.auth import AuthMiddlewareStack
+from    django.urls import re_path
+from    pong_app.consumers import *
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pong_game.settings')
 
+websocket_urlpatterns = [
+    re_path(r'ws/some_path/$', PongConsumer.as_asgi()),
+]
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
+    'websocket': URLRouter(websocket_urlpatterns),
+    # TODO voir si cest utile
+    # "websocket": AuthMiddlewareStack(
+        # URLRouter(
+            # websocket_urlpatterns
+        # )
+    # ),
 })
--- django/pong_game/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/some_path/$', consumers.PongConsumer.as_asgi()),
-]
