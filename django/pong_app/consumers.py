34333e2cee75452a3745703be86ae700327b0c5a
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-17 14:29:19 +0100

    add websocket connection in main.js

--- django/pong_app/consumers.py
+++ django/pong_app/consumers.py
@@ -1,21 +1,12 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
-import json
+from    channels.generic.websocket import AsyncWebsocketConsumer
+import  json
 
 class PongConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
-        print('connected')
-        await self.send(text_data=json.dumps({
-            'type': 'echo',
-            'message': 'Htllo',
-        }))
 
     async def disconnect(self, close_code):
-        print('disconnected')
+        pass
 
     async def receive(self, text_data):
-        message = json.loads(text_data)
-        await self.send(text_data=json.dumps({
-            'type': 'echo',
-            'message': message['message'],
-        }))
+        await self.send(text_data=text_data)
