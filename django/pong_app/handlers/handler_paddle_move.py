787d959200411fb74930c468bcf3a0307b652814
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-20 11:55:45 +0100

    add wait inside server (working)

--- django/pong_app/handlers/handler_paddle_move.py
+++ django/pong_app/handlers/handler_paddle_move.py
@@ -4,7 +4,6 @@ import asyncio
 async def keydown_loop(direction, consumer):
 	step = 10;
 
-	print(f"Moving: {consumer.moving}")
 	while (consumer.moving):
 		if (direction == 'ArrowUp' and consumer.paddle_position > 0):
 			consumer.paddle_position = consumer.paddle_position - step;
@@ -12,12 +11,11 @@ async def keydown_loop(direction, consumer):
 		elif (direction == 'ArrowDown' and consumer.paddle_position < consumer.canvas_height - 100):
 			consumer.paddle_position = consumer.paddle_position + step;
 			await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position}))
-		await asyncio.sleep(0.05)
+		await asyncio.sleep(0.02) # TODO change to fps
 
 async def handle_paddle_move(message, consumer):
 	if (message['key'] == 'keydown'):
 		consumer.moving = True;
-		await keydown_loop(message['direction'], consumer)
+		asyncio.create_task(keydown_loop(message['direction'], consumer))
 	elif (message['key'] == 'keyup'):
-		print("keyup")
 		consumer.moving = False;
