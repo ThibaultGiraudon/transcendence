842a51aee9354fcf6f5a60493a5a2a2ca2e62f79
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-20 11:50:22 +0100

    add wait inside server

--- django/pong_app/consumers.py
+++ django/pong_app/consumers.py
@@ -1,11 +1,12 @@
 from    channels.generic.websocket import AsyncWebsocketConsumer
-from    .handlers.handler_paddle_move import *
-from    .handlers.handler_init_game import *
+from    .handlers.handler_paddle_move import handle_paddle_move
+from    .handlers.handler_init_game import handle_init_game
 import  json
 import  asyncio
 
 class PongConsumer(AsyncWebsocketConsumer):
     paddle_position = 0
+    moving = False;
 
     async def connect(self):
         await self.accept()
--- django/pong_app/handlers/handler_paddle_move.py
+++ django/pong_app/handlers/handler_paddle_move.py
@@ -1,14 +1,23 @@
 import json
+import asyncio
 
-async def handle_paddle_move(message, consumer):
-	direction = message['direction']
+async def keydown_loop(direction, consumer):
 	step = 10;
 
-	if (direction == 'ArrowUp'):
-		if (consumer.paddle_position > 0):
+	print(f"Moving: {consumer.moving}")
+	while (consumer.moving):
+		if (direction == 'ArrowUp' and consumer.paddle_position > 0):
 			consumer.paddle_position = consumer.paddle_position - step;
-		await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position}))
-	elif (direction == 'ArrowDown'):
-		if (consumer.paddle_position < consumer.canvas_height - 100):
+			await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position}))
+		elif (direction == 'ArrowDown' and consumer.paddle_position < consumer.canvas_height - 100):
 			consumer.paddle_position = consumer.paddle_position + step;
-		await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position}))
+			await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position}))
+		await asyncio.sleep(0.05)
+
+async def handle_paddle_move(message, consumer):
+	if (message['key'] == 'keydown'):
+		consumer.moving = True;
+		await keydown_loop(message['direction'], consumer)
+	elif (message['key'] == 'keyup'):
+		print("keyup")
+		consumer.moving = False;
