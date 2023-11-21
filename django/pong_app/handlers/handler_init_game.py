6b8ede986fe8f92cdcbaa162c8f46486df2e373c
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-21 19:25:26 +0100

    working a lil bit with 2 paddle

--- django/pong_app/handlers/handler_init_game.py
+++ django/pong_app/handlers/handler_init_game.py
@@ -4,4 +4,5 @@ async def handle_init_game(message, consumer):
 	consumer.canvas_width = message['canvas_width']
 	consumer.canvas_height = message['canvas_height']
 	consumer.paddle_position = consumer.canvas_height / 2
-	await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position}))
+	await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position, 'paddle': 'left'}))
+	await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position + 100, 'paddle': 'right'}))
--- django/pong_app/handlers/handler_paddle_move.py
+++ django/pong_app/handlers/handler_paddle_move.py
@@ -1,41 +1,41 @@
 import json
 import asyncio
 
-def keyup_reset(direction, consumer):
-	if (direction == 'ArrowUp'):
-		consumer.keyState[direction] = False;
-	elif (direction == 'ArrowDown'):
-		consumer.keyState[direction] = False;
+def keyup_reset(direction, paddle, consumer):
+	if (direction == 'up'):
+		consumer.keyState[paddle][direction] = False;
+	elif (direction == 'down'):
+		consumer.keyState[paddle][direction] = False;
 
-	if (consumer.tasksAsyncio[direction]):
-		consumer.tasksAsyncio[direction].cancel()
+	if (consumer.tasksAsyncio[paddle][direction]):
+		consumer.tasksAsyncio[paddle][direction].cancel()
 
-async def keydown_loop(direction, consumer):
+async def keydown_loop(direction, paddle, consumer):
 	step = 10;
 
-	if (direction == 'ArrowUp'):
-		consumer.keyState['ArrowDown'] = False;
-	elif (direction == 'ArrowDown'):
-		consumer.keyState['ArrowUp'] = False;
+	if (direction == 'up'):
+		consumer.keyState[paddle]['down'] = False;
+	elif (direction == 'down'):
+		consumer.keyState[paddle]['up'] = False;
 
-	while (consumer.keyState[direction] or consumer.keyState[direction]):
-		if (consumer.keyState[direction] and direction == 'ArrowUp' and consumer.paddle_position > 0):
+	while (consumer.keyState[paddle][direction] or consumer.keyState[paddle][direction]):
+		if (consumer.keyState[paddle][direction] and direction == 'up' and consumer.paddle_position > 0):
 			consumer.paddle_position = consumer.paddle_position - step;
-		elif (consumer.keyState[direction] and direction == 'ArrowDown' and consumer.paddle_position < consumer.canvas_height - 100):
+		elif (consumer.keyState[paddle][direction] and direction == 'down' and consumer.paddle_position < consumer.canvas_height - 100):
 			consumer.paddle_position = consumer.paddle_position + step;
 
-		await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position}))
+		await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position, 'paddle': paddle}))
 		await asyncio.sleep(0.01) # TODO change to fps
 
 async def handle_paddle_move(message, consumer):
 	direction = message['direction']
+	paddle = message['paddle']
 	if (message['key'] == 'keydown'):
-		if (direction == 'ArrowUp'):
-			consumer.keyState[direction] = True;
-		elif (direction == 'ArrowDown'):
-			consumer.keyState[direction] = True;
-
-		consumer.tasksAsyncio[direction] = asyncio.create_task(keydown_loop(direction, consumer))
+		if (direction == 'up'):
+			consumer.keyState[paddle][direction] = True;
+		elif (direction == 'down'):
+			consumer.keyState[paddle][direction] = True;
+		consumer.tasksAsyncio[paddle][direction] = asyncio.create_task(keydown_loop(direction, paddle ,consumer))
 
 	elif (message['key'] == 'keyup'):
-		keyup_reset(direction, consumer)
+		keyup_reset(direction, paddle, consumer)
