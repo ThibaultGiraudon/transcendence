123279b90253f86f849c7f3e06e2f55949309a70
LeonPupier <leonpupier@gmail.com>
2023-11-21 11:57:23 +0100

    Working on authentification: login/out worked

--- back_end/pong_app/views.py
+++ back_end/pong_app/views.py
@@ -1,31 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db import connection
 from pong_app.models import PongGameState
 
-def homePage(request):
-    return render(request, 'homePage.html')
+def home(request):
+	return render(request, 'home.html')
 
-def pongGame(request):
-    game_state, created = PongGameState.objects.get_or_create(pk=1)
+def pong(request):
+	# Check if the user is already connected
+	if not request.user.is_authenticated:
+		return redirect('sign_in')
+	
+	game_state, created = PongGameState.objects.get_or_create(pk=1)
 
-    # Traitez les événements ou la logique du jeu ici
-    # Pour cet exemple, nous utilisons simplement la position de la raquette
-    if request.method == 'POST':
-        if 'up' in request.POST and game_state.paddle_position > 0:
-            game_state.paddle_position -= 10
-        elif 'down' in request.POST and game_state.paddle_position < 500:
-            game_state.paddle_position += 10
+	# Traitez les événements ou la logique du jeu ici
+	# Pour cet exemple, nous utilisons simplement la position de la raquette
+	if request.method == 'POST':
+		if 'up' in request.POST and game_state.paddle_position > 0:
+			game_state.paddle_position -= 10
+		elif 'down' in request.POST and game_state.paddle_position < 500:
+			game_state.paddle_position += 10
 
-        # Enregistrez la nouvelle position de la raquette dans la base de données
-        game_state.save()
+		# Enregistrez la nouvelle position de la raquette dans la base de données
+		game_state.save()
 
-    return render(request, 'pong_elements/index.html', {'game_state': game_state})
+	return render(request, 'pong_elements/pong.html', {'game_state': game_state})
 
 def testDBConnection(request):
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT 1")
-        connection.close()
-        return render(request, 'success.html')
-    except Exception as error:
-        return render(request, 'error.html')
+	try:
+		with connection.cursor() as cursor:
+			cursor.execute("SELECT 1")
+		connection.close()
+		return render(request, 'success.html')
+	except Exception as error:
+		return render(request, 'error.html')
