1eed08c8105c83abbd062eb0f6809247e50f2cd9
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-21 18:06:48 +0100

    norm handler

--- django/pong_app/handlers/handler_paddle_move.py
+++ django/pong_app/handlers/handler_paddle_move.py
@@ -8,8 +8,6 @@ async def keydown_loop(direction, consumer):
 		consumer.moving_down = False;
 	elif (direction == 'ArrowDown'):
 		consumer.moving_up = False;
-	print(f"keydown_loop moving_up: {consumer.moving_up}")
-	print(f"keydown_loop moving_down: {consumer.moving_down}")
 
 	while (consumer.moving_up or consumer.moving_down):
 		if (consumer.moving_up and direction == 'ArrowUp' and consumer.paddle_position > 0):
@@ -18,21 +16,24 @@ async def keydown_loop(direction, consumer):
 			consumer.paddle_position = consumer.paddle_position + step;
 
 		await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position}))
-		await asyncio.sleep(0.02) # TODO change to fps
+		await asyncio.sleep(0.01) # TODO change to fps
 
 async def handle_paddle_move(message, consumer):
 	if (message['key'] == 'keydown'):
 		if (message['direction'] == 'ArrowUp'):
 			consumer.moving_up = True;
 			consumer.moving_up_task = asyncio.create_task(keydown_loop(message['direction'], consumer))
+
 		elif (message['direction'] == 'ArrowDown'):
 			consumer.moving_down = True;
 			consumer.moving_down_task = asyncio.create_task(keydown_loop(message['direction'], consumer))
+
 	elif (message['key'] == 'keyup'):
 		if (message['direction'] == 'ArrowUp'):
 			consumer.moving_up = False;
 			if consumer.moving_up_task:
 				consumer.moving_up_task.cancel()
+
 		elif (message['direction'] == 'ArrowDown'):
 			consumer.moving_down = False;
 			if consumer.moving_down_task:
