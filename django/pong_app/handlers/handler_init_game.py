11abda834f816935f78c7a107d729adecdfc192e
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-22 14:17:41 +0100

    fix working

--- django/pong_app/handlers/handler_init_game.py
+++ django/pong_app/handlers/handler_init_game.py
@@ -1,8 +1,5 @@
 import json
 
 async def handle_init_game(message, consumer):
-	consumer.canvas_width = message['canvas_width']
-	consumer.canvas_height = message['canvas_height']
-	consumer.paddle_position = consumer.canvas_height / 2
-	await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position, 'paddle': 'left'}))
-	await consumer.send(json.dumps({'type': 'update_paddle_position', 'position': consumer.paddle_position + 100, 'paddle': 'right'}))
+	consumer.canvasInfo['width'] = message['canvas_width']
+	consumer.canvasInfo['height'] = message['canvas_height']
