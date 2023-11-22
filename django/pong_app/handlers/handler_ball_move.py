fecb77d563e8ffe8dd8ef021c2986d82791e4a21
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-11-22 16:24:12 +0100

    add collides (disabled here)

--- django/pong_app/handlers/handler_ball_move.py
+++ django/pong_app/handlers/handler_ball_move.py
@@ -1,11 +1,29 @@
 import	json
+import	asyncio
+import	math
 
-async def handle_ball_move(message, consumer):
-	print(message)
-	consumer.ballPosition['x'] += 100;
-	consumer.ballPosition['y'] += 100;
+async def sendUpdateMessage(consumer):
 	message = {
 		'type': 'update_ball_position',
 		'position': consumer.ballPosition,
 	}
 	await consumer.send(json.dumps(message))
+
+async def handle_ball_move(message, consumer):
+	print(message)
+	# consumer.ballPosition['x'] += 100;
+	# consumer.ballPosition['y'] += 100;
+	while (True):
+		delta_x = consumer.ballPosition['speed'] * math.cos(consumer.ballPosition['angle'])
+		delta_y = consumer.ballPosition['speed'] * math.sin(consumer.ballPosition['angle'])
+		consumer.ballPosition['x'] += delta_x
+		consumer.ballPosition['y'] += delta_y
+		if (consumer.ballPosition['x'] <= 0) or (consumer.ballPosition['x'] >= consumer.canvasInfo['width']):
+			consumer.ballPosition['angle'] = math.pi - consumer.ballPosition['angle']
+
+		if (consumer.ballPosition['y'] <= 0) or (consumer.ballPosition['y'] >= consumer.canvasInfo['height']):
+			consumer.ballPosition['angle'] = -consumer.ballPosition['angle']
+
+
+		await asyncio.sleep(0.1)
+		await sendUpdateMessage(consumer)
