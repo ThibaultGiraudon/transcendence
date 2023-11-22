fb74415ac951b5ab67d1d18862e12165059c4807
Thibault Giraudon <tgiraudo@z3r7p3.42lyon.fr>
2023-11-22 16:12:08 +0100

    create user with CustomUser

--- back_end/users_app/models.py
+++ back_end/users_app/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password, **extra_fields):
+    def create_user(self, email, username, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, username=username, password=password, **extra_fields)
-        user.save(self._db)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
         return user
 
     def create_superuser(self, email, username, password=None, **extra_fields):
@@ -16,11 +16,10 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
 
         return self.create_user(email, username, password=password, **extra_fields)
-    
+
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, unique=True)
-    password = models.CharField(max_length=30, blank=True)
 
     # Utilise le gestionnaire d'utilisateurs personnalisé
     objects = CustomUserManager()
@@ -34,6 +33,3 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
     
-
-class	PongGameState(models.Model):
-    paddle_position = models.IntegerField(default=0)
