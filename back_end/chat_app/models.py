287a5cef92c205e1752051cf42b3db9452bd08c3
Thibault Giraudon <tgiraudo@z3r8p1.42lyon.fr>
2023-11-28 13:51:57 +0100

    private msg

--- back_end/chat_app/models.py
+++ back_end/chat_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from users_app.models import CustomUser
 
-# Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(CustomUser, related_name='sender', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(CustomUser, related_name='receiver', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
--- back_end/chat_app/views.py
+++ back_end/chat_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from users_app.models import CustomUser
+from django.db import models
+from .models import Message
 
-# Create your views here.
+@login_required
+def chat_room(request, username):
+    other_user = get_object_or_404(CustomUser, username=username)
+    messages = Message.objects.filter(
+        (models.Q(sender=request.user) & models.Q(receiver=other_user)) |
+        (models.Q(sender=other_user) & models.Q(receiver=request.user))
+    ).order_by('timestamp')
+
+    return render(request, 'chat/chat_room.html', {'messages': messages, 'other_user': other_user})
+
+@login_required
+def send_message(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        receiver_username = request.POST.get('receiver_username')
+        receiver = get_object_or_404(CustomUser, username=receiver_username)
+
+        Message.objects.create(sender=request.user, receiver=receiver, content=content)
+
+        return JsonResponse({'status': 'OK'})
