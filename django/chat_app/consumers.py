79ad7add05016ca7cee6ae3cf1763d4e26b7ebd4
LeonPupier <leonpupier@gmail.com>
2023-11-30 09:27:10 +0100

    clear code

--- django/chat_app/consumers.py
+++ django/chat_app/consumers.py
@@ -7,14 +7,10 @@ messages = {}
 class ChatConsumer(AsyncWebsocketConsumer):
     
     async def connect(self):
-        logging.info("----------------\nCONNECT")
-        logging.info("coucou1")
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
         # Join room group
-        logging.info("----------------\nCONNECT")
-        logging.info("coucou2")
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
 
@@ -26,14 +22,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
             
     async def disconnect(self, close_code):
         # Leave room group
-        logging.info("----------------\nDISCONNECT")
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        logging.info("----------------\nRECEIVE")
-        logging.info(text_data_json)
         message = text_data_json.get("message")
         sender = text_data_json.get("sender")
 
@@ -46,8 +39,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def chat_message(self, event):
         message = event.get("message")
         sender = event.get("sender")
-        logging.info("----------------\nCHAT_MESSAGE")
-        logging.info(self.room_name)
 
         if self.room_name not in messages:
             messages[self.room_name] = []
