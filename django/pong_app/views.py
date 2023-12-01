bbe89f842a74f0764f293e2da8bd414698e1da24
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-12-01 15:49:10 +0100

    fix old version in views

--- django/pong_app/views.py
+++ django/pong_app/views.py
@@ -6,28 +6,12 @@ def home(request):
 	return render(request, 'home.html')
 
 def pong(request):
-	# Check if the user is already connected
 	if not request.user.is_authenticated:
 		return redirect('sign_in')
 
-	# Check arguments
 	if request.method == 'GET' and 'error' in request.GET:
 		return redirect('sign_in')
-	
-	game_state, created = PongGameState.objects.get_or_create(pk=1)
-
-	# Traitez les événements ou la logique du jeu ici
-	# Pour cet exemple, nous utilisons simplement la position de la raquette
-	if request.method == 'POST':
-		if 'up' in request.POST and game_state.paddle_position > 0:
-			game_state.paddle_position -= 10
-		elif 'down' in request.POST and game_state.paddle_position < 500:
-			game_state.paddle_position += 10
-
-		# Enregistrez la nouvelle position de la raquette dans la base de données
-		game_state.save()
-
-	return render(request, 'pong_elements/pong.html', {'game_state': game_state})
+	return render(request, 'pong_elements/pong.html')
 
 def testDBConnection(request):
 	try:
