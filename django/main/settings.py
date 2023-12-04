ef653979950fc28daa1d982cddcabce3f470aaa8
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-12-04 16:53:38 +0100

    work only in http

--- django/main/settings.py
+++ django/main/settings.py
@@ -14,11 +14,17 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# 
+# STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'pong_app', 'static'),
+# ]
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'pong_app', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Quick-start development settings - unsuitable for production
