9d4b6ea9d055ccf99c448133ea462c19e735c5ac
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-12-14 18:07:09 +0100

    clean some code

--- django/mainApp/apps.py
+++ django/mainApp/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class PongAppConfig(AppConfig):
+class MainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mainApp'
