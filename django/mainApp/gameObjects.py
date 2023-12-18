d9e64d59149a9306a3e634d0183cf1741507a10d
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-12-18 14:59:25 +0100

    change size to paddleSize

--- django/mainApp/gameObjects.py
+++ django/mainApp/gameObjects.py
@@ -11,7 +11,7 @@ class GameSettings:
 
         for id in range(self.nbPaddles):
             self.paddles.append(Paddle(id))
-            self.paddles[id].position = self.squareSize / 2 - self.paddles[id].size / 2
+            self.paddles[id].position = self.squareSize / 2 - self.paddles[id].paddleSize / 2
         
         if self.nbPaddles == 2:
             self.initPaddles2()
@@ -42,7 +42,7 @@ class Paddle:
         self.offset = 0
         self.position = 0
         self.paddleThickness = 20
-        self.size = 100
+        self.paddleSize = 100
         self.speed = 10
         self.score = 0
         self.keyState = {
@@ -93,31 +93,18 @@ class Ball:
     def checkPaddleCollision(self, paddle):
         # TODO a voir si on inverse pas des le debut plutot que inverser chaque fois
         if (paddle.id == 2 or paddle.id == 3):
-            paddleThickness, size = paddle.size, paddle.paddleThickness
+            paddleThickness, paddleSize = paddle.paddleSize, paddle.paddleThickness
             offset, position = paddle.position, paddle.offset
         else:
-            paddleThickness, size = paddle.paddleThickness, paddle.size
+            paddleThickness, paddleSize = paddle.paddleThickness, paddle.paddleSize
             offset, position = paddle.offset, paddle.position
 
         closestX = max(offset, min(self.x, offset + paddleThickness))
-        closestY = max(position, min(self.y, position + size))
+        closestY = max(position, min(self.y, position + paddleSize))
         distance = math.sqrt((self.x - closestX)**2 + (self.y - closestY)**2)
 
         if distance < self.radius:
             print("collisions paddle : ", paddle.id)
-
-        # if (distance <= self.radius):
-        #     print("collisions paddle : ", paddle.id)
-        #     collisionPosition = (closestY - paddle.position) / paddle.size
-        #     reflectionAngle = (collisionPosition - 0.5) * math.pi
-        #     maxAngle = math.pi / 3
-
-        #     if (paddle.id == 0):
-        #         self.angle = max(-maxAngle, min(maxAngle, reflectionAngle))
-        #     elif (paddle.id == 1):
-        #         self.angle = math.pi - max(-maxAngle, min(maxAngle, reflectionAngle))
-
-        #     self.__powerShot(paddle, collisionPosition)
 
     def checkWallCollision(self, gameSettings):
         id = -1
