9ddf6d14fadd08c0b9da4073a8c7997d0024ad69
LeonPupier <leonpupier@gmail.com>
2023-12-20 09:38:58 +0100

    clear code

--- django/mainApp/views/usersView.py
+++ django/mainApp/views/usersView.py
@@ -1,4 +1,4 @@
-import logging
+import logging, os
 import requests
 from PIL import Image
 from io import BytesIO
@@ -18,22 +18,13 @@ from ..models import Notification, Channel
 
 # 42 API
 API_USER = 'https://api.intra.42.fr/v2/me'
-token = '690c107e335181f7039f3792799aebb1fa4d55b320bd232dd68877c0cc13545d'
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 
 
 # Functions
 @csrf_protect
 def sign_in(request):
-	"""
-	Try to login in the user
- 
-	Arguments:
-		request: ???
-	Returns:
-		pong page : the credential are good
-		sign_in page : the credential are wrong
-	"""
-
 	if request.method == 'GET':
 		form = LoginForm()
 		return render(request, 'users/sign_in.html', {'form': form})
@@ -46,10 +37,11 @@ def sign_in(request):
 			password = form.cleaned_data['password']
 			user = authenticate_custom_user(email=email, password=password)
 			if user:
+				# Update the user status
 				user.status = "online"
 				user.save()
+				# Send the status to the channel layer
 				channel_layer = get_channel_layer()
-
 				async_to_sync(channel_layer.group_send)(
 					'status',
 					{
@@ -72,16 +64,6 @@ def sign_in(request):
 
 @csrf_protect
 def sign_up(request):
-	"""
-	Create a new user
- 
-	Arguments:
-		request: ???
-	Returns:
-		sign_in page : all fields are filled
-		sign_up page : an error occured
-	"""
-
 	if request.method == 'GET':
 		form = SignUpForm()
 		return render(request, 'users/sign_up.html', {'form': form})
@@ -90,7 +72,6 @@ def sign_up(request):
 		form = SignUpForm(request.POST)
 
 		if form.is_valid():
-
 			if CustomUser.objects.filter(email=form.cleaned_data['email']).exists():
 				messages.error(request, "This email is already taken")
 				return redirect('sign_up')
@@ -100,15 +81,15 @@ def sign_up(request):
 			elif len(form.cleaned_data['username']) < 4:
 				messages.error(request, "Your username is too short (4 characters minimum)")
 				return redirect('sign_up')
-			
+			# Create the user
 			user = CustomUser.objects.create_user(
 					username=form.cleaned_data['username'],
 					email=form.cleaned_data['email'],
 					password=form.cleaned_data['password'])
 			user.save()
 			login(request, user)
+			# Send the status to the channel layer
 			channel_layer = get_channel_layer()
-
 			async_to_sync(channel_layer.group_send)(
 				'status',
 				{
@@ -141,20 +122,10 @@ def sign_up(request):
 
 
 def sign_out(request):
-	"""
-	Log out the user
- 
-	Arguments:
-		request: ???
-	Returns:
-		sign_in page
-	"""
-
 	request.user.status = "offline"
 	request.user.save()
- 
+	# Send the status to the channel layer
 	channel_layer = get_channel_layer()
-
 	async_to_sync(channel_layer.group_send)(
 		'status',
 		{
@@ -174,23 +145,13 @@ def sign_out(request):
 def ft_api(request):
 	protocol = request.scheme
 	port = '%3A8001' if protocol == "https" else '%3A8000'
-	api_url = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-4bc482d21834a4addd9108c8db4a5f99efb73b172f1a4cb387311ee09a26173c&redirect_uri=" + \
+	api_url = "https://api.intra.42.fr/oauth/authorize?client_id=" + CLIENT_ID + "&redirect_uri=" + \
 	protocol + "%3A%2F%2Flocalhost" + \
 	port + "%2Fcheck_authorize%2F&response_type=code"
 	return redirect(api_url)
 
 
 def	check_authorize(request):
-	"""
-	Check if the user authorize the 42's connection or not
- 
-	Arguments:
-		request: ???
-	Returns:
-		pong page : the user authorize the connection
-		sign_in page : the user refuse the connection
-	"""
-
 	if request.method == 'GET' and 'error' in request.GET:
 		return redirect('sign_in')
 	if request.method == 'GET' and 'code' in request.GET:
@@ -202,14 +163,6 @@ def	check_authorize(request):
 
 
 def	connect_42_user(request, response_data):
-	"""
-	Create user if the user connect for the first time
-
-	Arguments:
-		request : ????
-		response_data : json data
-	"""
-
 	user = authenticate_42_user(email=response_data['email'])
 	if user:
 		user.status = "online"
@@ -253,18 +206,6 @@ def	connect_42_user(request, response_data):
 
 
 def make_api_request_with_token(api_url, token):
-	"""
-	Request for 42 api
- 
-	Arguments:
-		api_url : the api url request
-		token : token to access the api
-
-	Returns:
-		None : the request faild
-		json data : the request succeed
-	"""
-
 	headers = {
 		'Authorization': f'Bearer {token}',
 	}
@@ -285,23 +226,13 @@ def make_api_request_with_token(api_url, token):
 
 
 def handle_42_callback(request, code):
-	"""
-	Ask 42 api for token thanks to the code replied after the redirection
- 
-	Arguments:
-		request : ???
-		code : the replied code from 42 redirection
-	Returns:
-		None : the request failed
-		token : the requet succeed
-	"""
 	port = '8001' if request.scheme == 'https' else '8000'
 	redirect_uri = request.scheme + '://localhost:' + port + '/check_authorize/'
 	token_url = "https://api.intra.42.fr/oauth/token"
 	token_params = {
 		'grant_type': 'authorization_code',
-		'client_id': 'u-s4t2ud-4bc482d21834a4addd9108c8db4a5f99efb73b172f1a4cb387311ee09a26173c',
-		'client_secret': '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',
+		'client_id': CLIENT_ID,
+		'client_secret': CLIENT_SECRET,
 		'code': code,
 		'redirect_uri': redirect_uri
 	}
@@ -319,18 +250,6 @@ def handle_42_callback(request, code):
 
 
 def authenticate_custom_user(email, password):
-	"""
-	Authenticate function with email and password
- 
-	Arguments:
-		email : the email of the user who want to connect
-		password : the password of the user who want to connect
-
-	Returns:
-		None : the user isn't registered
-		user : the user exist
-	"""
-
 	User = get_user_model()
 
 	try:
@@ -342,17 +261,6 @@ def authenticate_custom_user(email, password):
 
 
 def authenticate_42_user(email):
-	"""
-	Authenticate function without password
- 
-	Arguments:
-		email : the email of the 42 user who want to connect
-
-	Returns:
-		None : the user isn't registered
-		user : the user exist
-	"""
-
 	User = get_user_model()
 
 	try:
@@ -374,31 +282,21 @@ def profile(request, username):
 		return redirect('sign_in')
 
 	photo_name = request.user.photo.name
-	User = get_user_model()
 
+	# get the user
+	User = get_user_model()
 	try:
 		user = User.objects.get(username=username)
 	except User.DoesNotExist:
 		return redirect('users')
 
-	# get the room_name where the user is
+	# get the room_name where the user and I are
 	room = None
 	channels = list(request.user.channels.all())
 	for channel in channels:
 		if channel.other_name == user.username or channel.name == user.username:
 			room = channel.room_name
 			break
-	# try:
-	# 	# get the room where only the user and I are
-	# 	from django.db.models import Count
-	# 	channel = Channel.objects.annotate(user_count=Count('users')).filter(user_count=2)
-	# 	logging.info("-------------------")
-	# 	logging.info(channel)
-	# 	if not channel is None:
-	# 		room = channel.name
-	# 	logging.info(room)
-	# except Channel.DoesNotExist:
-	# 	logging.info("No room found")
 
 	if request.method == 'GET':
 		form = EditProfileForm(instance=request.user)
@@ -419,10 +317,10 @@ def profile(request, username):
 			elif len(form.cleaned_data['username']) < 4:
 				messages.error(request, "Your username is too short (4 characters minimum)")
 				return redirect('profile', username=username)
-	
 			form.save()
 			messages.success(request, 'Your informations have been updated')
 			return redirect('profile', username=request.user.username)
+		
 		else:
 			if 'photo' in form.errors:
 				messages.error(request, 'Please enter a valid picture')
@@ -439,12 +337,15 @@ def users(request):
 	if not request.user.is_authenticated:
 		return redirect('sign_in')
 	
+	# Get all users and the friends
 	User = get_user_model()
 	all_users = User.objects.all()
+
 	friends = []
 	for user in all_users:
 		if user.id in request.user.follows:
 			friends.append(user)
+
 	context = {'all_users':all_users, 'friends':friends}
 
 	if request.method == 'GET':
@@ -457,6 +358,7 @@ def follow(request, id):
 	if not request.user.is_authenticated:
 		return redirect('sign_in')
 
+	# Check if the user exist and if he is not already followed
 	User = get_user_model()
 	try:
 		userTo = User.objects.get(id=id)
@@ -478,6 +380,7 @@ def unfollow(request, id):
 	if not request.user.is_authenticated:
 		return redirect('sign_in')
 	
+	# Check if the user exist and if he is followed
 	User = get_user_model()
 	try:
 		userTo = User.objects.get(id=id)
