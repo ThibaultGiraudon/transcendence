e1c5d85770f1105745aa598ab20d3a97756c56b4
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-12-20 18:10:37 +0100

    fix SPA

--- django/static/js/main.js
+++ django/static/js/main.js
@@ -5,12 +5,23 @@ function navigateTo(event, route) {
 
   // Utilisez fetch() ou toute autre méthode pour récupérer le contenu de la route
   // et mettez à jour le contenu du conteneur principal
-//   fetch(route)
-//     .then(response => response.text())
-//     .then(html => {
-//       document.getElementById('app').innerHTML = html;
-//     });
+  fetch(route)
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById('app').innerHTML = html;
+    });
 
-//   // Facultatif : Mettez à jour l'URL du navigateur
-//   history.pushState(null, null, route);
+  // Facultatif : Mettez à jour l'URL du navigateur
+  history.pushState(null, null, route);
 }
+
+window.addEventListener('popstate', function(event) {
+  // Utilisez event.state pour obtenir les données d'état si nécessaire
+  // Ensuite, récupérez l'URL actuelle et mettez à jour le contenu en conséquence
+  const currentUrl = window.location.pathname;
+  fetch(currentUrl)
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById('app').innerHTML = html;
+    });
+});
