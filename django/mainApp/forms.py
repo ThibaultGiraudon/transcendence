2cfa442630b57c49c0fe81d67d634d6aaad82553
LeonPupier <leonpupier@gmail.com>
2024-01-02 14:40:37 +0100

    Patch signUp form with AJAX and CSRF Token

--- django/mainApp/forms.py
+++ django/mainApp/forms.py
@@ -7,6 +7,7 @@ class LoginForm(forms.Form):
 	email = forms.EmailField(widget=forms.EmailInput)
 	password = forms.CharField(max_length=65, widget=forms.PasswordInput)
 
+
 class SignUpForm(forms.Form):
 	username = forms.CharField(
 		max_length=65, 
@@ -20,6 +21,23 @@ class SignUpForm(forms.Form):
 	)
 	email = forms.EmailField(widget=forms.EmailInput)
 	password = forms.CharField(max_length=65, widget=forms.PasswordInput)
+
+	def clean(self):
+		cleaned_data = super().clean()
+		username = cleaned_data.get('username')
+		email = cleaned_data.get('email')
+
+		if CustomUser.objects.filter(email=email).exists():
+			self.add_error('email', "This email is already taken")
+
+		if CustomUser.objects.filter(username=username).exists():
+			self.add_error('username', "This username is already taken")
+
+		if len(username) < 4:
+			self.add_error('username', 'Your username is too short (4 characters minimum)')
+
+		return cleaned_data
+
 
 class EditProfileForm(forms.ModelForm):
    
--- django/mainProject/settings.py
+++ django/mainProject/settings.py
@@ -132,6 +132,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
+CSRF_COOKIE_HTTPONLY = False
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
