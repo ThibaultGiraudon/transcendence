cfe9dd66e284f8b759289bfaa3a56a33967f21fb
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-01-10 15:02:04 +0100

    save tmp

--- django/static/pong_app/js/main.js
+++ django/static/pong_app/js/main.js
@@ -1,11 +1,9 @@
-import * as utils from './utils.js';
-
 // TODO separer dans des fichiers differents
 
-let     websocketProtocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-let     websocketPort = window.location.protocol === 'https:' ? ':8001' : ':8000';
-const   socketUrl = websocketProtocol + '//' + window.location.hostname + websocketPort + '/ws/some_path/';
-const   socket = new WebSocket(socketUrl);
+// let     websocketProtocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+// let     websocketPort = window.location.protocol === 'https:' ? ':8001' : ':8000';
+// const   socketUrl = websocketProtocol + '//' + window.location.hostname + websocketPort + '/ws/some_path/';
+// const   socket = new WebSocket(socketUrl);
 
 const   keyState = {
     o: false,
@@ -18,104 +16,104 @@ const   keyState = {
     m: false,
 };
 
-// EVENTS
-document.addEventListener('DOMContentLoaded', function() {    
-    document.addEventListener('keydown', function(event) {
-        if (!keyState[event.key] && keyState.hasOwnProperty(event.key)) {
-            keyState[event.key] = true;
-            const message = {
-                type: 'paddle_move',
-                key: 'keydown',
-                direction: utils.getPaddleDirection(event.key),
-                id: utils.getPaddleID(event.key),
-            };
-            socket.send(JSON.stringify(message));
-        }
+// // EVENTS
+// document.addEventListener('DOMContentLoaded', function() {    
+//     document.addEventListener('keydown', function(event) {
+//         if (!keyState[event.key] && keyState.hasOwnProperty(event.key)) {
+//             keyState[event.key] = true;
+//             const message = {
+//                 type: 'paddle_move',
+//                 key: 'keydown',
+//                 direction: getPaddleDirection(event.key),
+//                 id: getPaddleID(event.key),
+//             };
+//             socket.send(JSON.stringify(message));
+//         }
 
-        if (event.key === "ArrowUp" || event.key === "ArrowDown") {
-            event.preventDefault();
-        }
-    });
+//         if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+//             event.preventDefault();
+//         }
+//     });
 
-    document.addEventListener('keyup', function(event) {
-        if (keyState.hasOwnProperty(event.key)) {
-            keyState[event.key] = false;
-            const message = {
-                type: 'paddle_move',
-                key: 'keyup',
-                direction: utils.getPaddleDirection(event.key),
-                id: utils.getPaddleID(event.key),
-            };
-            socket.send(JSON.stringify(message));
-        }
-    });
+//     document.addEventListener('keyup', function(event) {
+//         if (keyState.hasOwnProperty(event.key)) {
+//             keyState[event.key] = false;
+//             const message = {
+//                 type: 'paddle_move',
+//                 key: 'keyup',
+//                 direction: getPaddleDirection(event.key),
+//                 id: getPaddleID(event.key),
+//             };
+//             socket.send(JSON.stringify(message));
+//         }
+//     });
 
-    socket.addEventListener('open', (event) => {
-        console.log('Connected to websocket server');
-        const gameMode = window.gameMode;
-        console.log(gameMode);
-        const message = {
-            type: gameMode,
-        };
-        socket.send(JSON.stringify(message));
-    });
+//     socket.addEventListener('open', (event) => {
+//         console.log('Connected to websocket server');
+//         const gameMode = window.gameMode;
+//         console.log(gameMode);
+//         const message = {
+//             type: gameMode,
+//         };
+//         socket.send(JSON.stringify(message));
+//     });
 
-    socket.addEventListener('message', (event) => {
-        let message = JSON.parse(event.data);
+//     socket.addEventListener('message', (event) => {
+//         let message = JSON.parse(event.data);
 
-        if (message.type === 'init_game_size') {
-            initsquareSize(message);
-        }
+//         if (message.type === 'init_game_size') {
+//             initsquareSize(message);
+//         }
 
-        if (message.type === 'init_paddle_position') {
-            initPaddlePosition(message, elements.paddles[message.id]);
-        }
+//         if (message.type === 'init_paddle_position') {
+//             initPaddlePosition(message, elements.paddles[message.id]);
+//         }
 
-        if (message.type === 'init_score') {
-            initScore(message);
-        }
+//         if (message.type === 'init_score') {
+//             initScore(message);
+//         }
 
-        if (message.type === 'update_paddle_position') {
-            updatePaddlePosition(message);
-        }
+//         if (message.type === 'update_paddle_position') {
+//             updatePaddlePosition(message);
+//         }
 
-        if (message.type === 'update_ball_position') {
-            updateBallPosition(message);
-        }
+//         if (message.type === 'update_ball_position') {
+//             updateBallPosition(message);
+//         }
 
-        if (message.type === 'update_score') {
-            updateScore(message);
-        }
-    });
-});
+//         if (message.type === 'update_score') {
+//             updateScore(message);
+//         }
+//     });
+// });
 
 
-var phaserGame;
-var config = {
-    type: Phaser.AUTO,
-    width: 0,
-    height: 0,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
-    scene: {
-        create: create,
-        updatePaddlePosition: updatePaddlePosition,
-        updateBallPosition: updateBallPosition,
-    },
-    backgroundColor: '#212121',
-};
+// var phaserGame;
+// var config = {
+//     type: Phaser.AUTO,
+//     width: 0,
+//     height: 0,
+//     physics: {
+//         default: 'arcade',
+//         arcade: {
+//             gravity: { y: 0 },
+//             debug: false
+//         }
+//     },
+//     scene: {
+//         create: create,
+//         updatePaddlePosition: updatePaddlePosition,
+//         updateBallPosition: updateBallPosition,
+//     },
+//     backgroundColor: '#212121',
+// };
 
-const elements = {
-    scoreText: [],
-    paddles: [],
-    ball: null,
-    field: null,
-};
+// const elements = {
+//     scoreText: [],
+//     paddles: [],
+//     ball: null,
+//     field: null,
+// };
 
 function create() {
     elements.field = this.add.rectangle(0, 0, 0, 0, 0x404040, 0.4).setVisible(true);
@@ -198,3 +196,85 @@ function changeAllScores(message) {
         }
     }
 }
+
+function handleMutation() {
+    if (document.getElementById('game-page')) {
+        let     websocketProtocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+        let     websocketPort = window.location.protocol === 'https:' ? ':8001' : ':8000';
+        const   socketUrl = websocketProtocol + '//' + window.location.hostname + websocketPort + '/ws/some_path/';
+        const   socket = new WebSocket(socketUrl);
+
+        document.addEventListener('keydown', function(event) {
+            if (!keyState[event.key] && keyState.hasOwnProperty(event.key)) {
+                keyState[event.key] = true;
+                const message = {
+                    type: 'paddle_move',
+                    key: 'keydown',
+                    direction: getPaddleDirection(event.key),
+                    id: getPaddleID(event.key),
+                };
+                socket.send(JSON.stringify(message));
+            }
+
+            if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+                event.preventDefault();
+            }
+        });
+
+        document.addEventListener('keyup', function(event) {
+            if (keyState.hasOwnProperty(event.key)) {
+                keyState[event.key] = false;
+                const message = {
+                    type: 'paddle_move',
+                    key: 'keyup',
+                    direction: getPaddleDirection(event.key),
+                    id: getPaddleID(event.key),
+                };
+                socket.send(JSON.stringify(message));
+            }
+        });
+            
+        socket.addEventListener('open', (event) => {
+            const gameMode = document.querySelector('script[data-game-mode]').getAttribute('data-game-mode');
+            console.log(gameMode);
+            const message = {
+                type: gameMode,
+            };
+            socket.send(JSON.stringify(message));
+        });
+
+        socket.addEventListener('message', (event) => {
+            let message = JSON.parse(event.data);
+
+            if (message.type === 'init_game_size') {
+                initsquareSize(message);
+            }
+
+            if (message.type === 'init_paddle_position') {
+                initPaddlePosition(message, elements.paddles[message.id]);
+            }
+
+            if (message.type === 'init_score') {
+                initScore(message);
+            }
+
+            if (message.type === 'update_paddle_position') {
+                updatePaddlePosition(message);
+            }
+
+            if (message.type === 'update_ball_position') {
+                updateBallPosition(message);
+            }
+
+            if (message.type === 'update_score') {
+                updateScore(message);
+            }
+        });
+
+        console.log('game page');
+    }
+}
+
+// Observe changes on the chat page
+const observer = new MutationObserver(handleMutation);
+observer.observe(document, { childList: true, subtree: true });
