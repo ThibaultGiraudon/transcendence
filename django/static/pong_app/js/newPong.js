6ed0dcf779b14c182bc5f8c78f956a448184a23a
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-01-18 13:15:04 +0100

    add createField in game

--- django/static/pong_app/js/newPong.js
+++ django/static/pong_app/js/newPong.js
@@ -1,16 +1,31 @@
-function gameProcess() {
-	console.log("OK");
+function createGameField(gameContext, size) {
+	fieldSize = 30;
+    const gameField = {
+        x: fieldSize,
+        y: fieldSize,
+        width: size - fieldSize * 2,
+        height: size - fieldSize * 2
+    };
+    gameContext.fillStyle = "#404040";
+    gameContext.fillRect(gameField.x, gameField.y, gameField.width, gameField.height);
+}
+
+function createGameCanvas() {
+	const canvasSize = 800;
 
 	const gameCanvas = document.getElementById('gameCanvas');
-    const gameContext = gameCanvas.getContext('2d');
-	const canvasSize = 600;
-
     gameCanvas.width = canvasSize;
     gameCanvas.height = canvasSize;
-	console.log(gameCanvas.width);
 
+    const gameContext = gameCanvas.getContext('2d');
     gameContext.fillStyle = "#212121";
     gameContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
 
+	createGameField(gameContext, canvasSize);
 
+	return (gameCanvas, gameContext)	
+}
+
+function gameProcess() {
+	gameCanvas, gameContext = createGameCanvas();
 }
