d38cf3213d6417e56bbcbe7f0c3e01ec47ecca63
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-01-23 19:00:11 +0100

    add playerID

--- django/mainApp/consumers/gameConsumer.py
+++ django/mainApp/consumers/gameConsumer.py
@@ -1,11 +1,27 @@
 from    channels.generic.websocket import AsyncWebsocketConsumer
+# from	..models import Game
 from    ..pongFunctions.handlerInitGame import handle_init_game
 from    ..pongFunctions.handlerPaddleMove import handle_paddle_move
 from    ..pongFunctions.gameSettingsClass import GameSettings
 import  json
 
+async def handleInitGame(consumer, gameID, gameMode, playerID):
+	# try:
+		# game = Game.objects.get(id=gameID)
+	# except Game.DoesNotExist:
+		# return (False)
+	# if playerID not in game.playerList:
+	# 	return (False)
+	# consumer.playerList.append(playerID)
+
+	# for player in consumer.playerList:
+	# 	print(player)
+	
+	return (True)
+
 class GameConsumer(AsyncWebsocketConsumer):
 	gameSettings = GameSettings(800)
+	playerList = []
 
 	async def connect(self):
 		await self.channel_layer.group_add('game', self.channel_name)
@@ -17,12 +33,22 @@ class GameConsumer(AsyncWebsocketConsumer):
 		await self.channel_layer.group_discard('game', self.channel_name)
 
 	async def receive(self, text_data):
+		gameID = self.scope['url_route']['kwargs']['game_id']
 		message = json.loads(text_data)
 		print(message)
+
+		if (message['type'] == 'init_ranked_solo_game'):
+			await handleInitGame(self, gameID, message['type'], message['playerID'])
+
 		await self.channel_layer.group_send('game', {
 			'type': 'reload_page',
 			'message': 'reload'
 		})
+
+	async def init_ranked_solo_game(self, event):
+		print('init_ranked_solo_game')
+		message = json.dumps(event['message'])
+		print(message)
 
 	async def reload_page(self, event):
 		# Envoyez le message "reload" à tous les clients
