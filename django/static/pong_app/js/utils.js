bfc1ccc2e82aa4ddd4d0328f7834a21f37558463
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-01-25 16:20:09 +0100

    add send and unique gameSettings

--- django/static/pong_app/js/utils.js
+++ django/static/pong_app/js/utils.js
@@ -4,26 +4,12 @@ function getPaddleID(key) {
 	} else if (key === 'w' || key === 's') {
 		return '0';
 	}
-
-	// TODO tmp for debug
-	if (key === 'z' || key === 'x') {
-		return '2';
-	} else if (key === 'n' || key === 'm') {
-		return '3';
-	}
 }
 
 function getPaddleDirection(key) {
 	if (key === 'o' || key === 'w') {
 		return 'up';
 	} else if (key === 'l' || key === 's') {
-		return 'down';
-	}
-
-	//  TODO tmp
-	if (key === 'z' || key === 'n') {
-		return 'up';
-	} else if (key === 'x' || key === 'm') {
 		return 'down';
 	}
 }
