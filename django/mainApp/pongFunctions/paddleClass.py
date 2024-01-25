57861871c711ce55dc4cae702718dd734d32eaf2
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-01-25 17:48:10 +0100

    add handlePaddleMove file add paddle Clear (fix)

--- django/mainApp/pongFunctions/paddleClass.py
+++ django/mainApp/pongFunctions/paddleClass.py
@@ -1,7 +1,32 @@
+# class Paddle:
+#     def __init__(self, id):
+#         self.id = id
+#         self.position = None
+#         self.speed = 10
+#         self.score = 0
+#         self.keyState = {
+#             'up': False,
+#             'down': False,
+#         }
+#         self.taskAsyncio = {
+#             'up': None,
+#             'down': None,
+#         }
+#         self.isAlive = False
+#         self.isAI = False
+#         self.aiTask = None
+
+#     def moveUp(self):
+#         self.position -= self.speed
+    
+#     def moveDown(self):
+#         self.position += self.speed
+
 class Paddle:
     def __init__(self, id):
         self.id = id
-        self.position = None
+        # self.offset = 0
+        self.position = 0
         self.speed = 10
         self.score = 0
         self.keyState = {
@@ -21,35 +46,3 @@ class Paddle:
     
     def moveDown(self):
         self.position += self.speed
-
-# class Paddle:
-#     def __init__(self, id):
-#         self.id = id
-#         self.offset = 0
-#         self.position = 0
-#         self.speed = 10
-#         self.score = 0
-#         self.keyState = {
-#             'up': False,
-#             'down': False,
-#         }
-#         self.taskAsyncio = {
-#             'up': None,
-#             'down': None,
-#         }
-#         self.colorArray = [
-#             "0xE21E59",
-#             "0x1598E9",
-#             "0x2FD661",
-#             "0xF19705",
-#         ]
-#         self.color = self.colorArray[self.id]
-#         self.isAlive = False
-#         self.isAI = False
-#         self.aiTask = None
-
-#     def moveUp(self):
-#         self.position -= self.speed
-    
-#     def moveDown(self):
-#         self.position += self.speed
