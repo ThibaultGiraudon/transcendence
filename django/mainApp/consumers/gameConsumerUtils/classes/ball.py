3ea14befe9945723c6f3c1b45e12560a8d505565
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-01-31 11:32:22 +0100

    add ball collisions

--- django/mainApp/consumers/gameConsumerUtils/classes/ball.py
+++ django/mainApp/consumers/gameConsumerUtils/classes/ball.py
@@ -27,6 +27,46 @@ class Ball:
                 randomAngle.append(math.pi / 2)
         return (randomAngle)
 
+    def __getReflectionAngle(self, paddle, maxAngle, reflectionAngle):
+        if (paddle.id == 0):
+            self.angle = max(-maxAngle, min(maxAngle, reflectionAngle))
+        elif (paddle.id == 1):
+            self.angle = math.pi - max(-maxAngle, min(maxAngle, reflectionAngle))
+        elif (paddle.id == 2):
+            self.angle = math.pi / 2 - max(-maxAngle, min(maxAngle, reflectionAngle))
+        elif (paddle.id == 3):
+            self.angle = -math.pi / 2 + max(-maxAngle, min(maxAngle, reflectionAngle))
+
+    def checkPaddleCollision(self, paddle, gameSettings):
+        if (paddle.isAlive == False):
+            return    
+
+        if (paddle.id == 2 or paddle.id == 3):
+            paddleThickness, paddleSize = gameSettings.paddleSize, gameSettings.paddleThickness
+            offset, position = paddle.position, paddle.offset
+        else:
+            paddleThickness, paddleSize = gameSettings.paddleThickness, gameSettings.paddleSize
+            offset, position = paddle.offset, paddle.position
+        
+
+        closestX = max(offset, min(self.x, offset + paddleThickness))
+        closestY = max(position, min(self.y, position + paddleSize))
+        distance = math.sqrt((self.x - closestX)**2 + (self.y - closestY)**2)
+        
+        print('distance', distance, 'paddle', paddle.id)
+
+        # if distance < self.radius:
+        #     if (paddle.id == 2 or paddle.id == 3):
+        #         collisionPosition = (closestX - offset) / paddleThickness
+        #     else:
+        #         collisionPosition = (closestY - position) / paddleSize
+        #     reflectionAngle = (collisionPosition - 0.5) * math.pi
+        #     maxAngle = math.pi / 3
+
+        #     self.__getReflectionAngle(paddle, maxAngle, reflectionAngle)
+            # TODO add powershot
+            # self.__powerShot(paddle, collisionPosition)
+
     def move(self):
         deltaX = self.speed * math.cos(self.angle) 
         deltaY = self.speed * math.sin(self.angle)
