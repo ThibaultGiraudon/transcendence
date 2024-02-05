f25464a4e4f6fbc961824f6e2b23473dde5494f3
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-02-05 12:21:28 +0100

    add score_div

--- django/static/pong/js/newPong.js
+++ django/static/pong/js/newPong.js
@@ -153,7 +153,25 @@ function updatePaddlePosition(paddleID, position) {
 	elements.paddles[paddleID].draw(position);
 }
 
-// TODO add radius from message
+function updateScore(message) {
+    const backgroundColors = ['#E21E59', '#1598E9', '#2FD661', '#F19705'];
+    const scoreSpans = document.querySelectorAll('.player_score');
+
+    if (message.nbPaddles == 2) {
+        scoreSpans[message.id].style.width = '50%';
+    } else if (message.nbPaddles == 4) {
+        scoreSpans[message.id].style.width = '25%';
+    }
+    scoreSpans[message.id].textContent = message.score;
+    scoreSpans[message.id].style.backgroundColor = backgroundColors[message.id];
+	if (message.score >= 10) {
+		scoreSpans[message.id].style.backgroundColor = '#212121';
+		scoreSpans[message.id].style.color = '#DADADA';
+		elements.paddles[message.id].clear();
+		elements.paddles[message.id].draw();
+	}
+}
+
 function updateBallPosition(x, y, color, radius) {
 	if (elements.ball) {
 		elements.ball.clear();
@@ -211,8 +229,7 @@ function gameProcess(isWaitingPage, gameMode, gameID, playerID) {
 		}
 
 		if (message.type === 'update_score') {
-			console.log(message);
-			// updatePaddlePosition(message.id, message.position);
+			updateScore(message);
 		}
 
 		if (message.type === 'update_paddle_position') {
