29abbf4634905cbb5e0bc59cf45c2641de841cbf
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-02-05 13:57:21 +0100

    fix score

--- django/mainApp/consumers/gameConsumerUtils/senders/sendUpdateScore.py
+++ django/mainApp/consumers/gameConsumerUtils/senders/sendUpdateScore.py
@@ -1,6 +1,6 @@
 async def sendUpdateScore(consumer, gameSettings):
 	for paddle in gameSettings.paddles:
-		if (paddle.isAlive == True):
+		if (paddle.id < gameSettings.nbPaddles):
 			await consumer.channel_layer.group_send('game', {
 				'type': 'update_score',
 				'id': paddle.id,
