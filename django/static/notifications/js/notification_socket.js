e2a17274912c7a67e0456e040c42ddda2df53828
LeonPupier <leonpupier@gmail.com>
2024-02-07 17:54:56 +0100

    websocket working

--- django/static/notifications/js/notification_socket.js
+++ django/static/notifications/js/notification_socket.js
@@ -1,34 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-	// Init the socket
-	let websocketProtocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-	let websocketPort = window.location.protocol === 'https:' ? ':8001' : ':8000';
-	const notificationSocketUrl = websocketProtocol + '//' + window.location.hostname + websocketPort + '/ws/notifications/';
+    // Init the socket
+    let websocketProtocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    let websocketPort = window.location.protocol === 'https:' ? ':8001' : ':8000';
+    const notificationSocketUrl = websocketProtocol + '//' + window.location.hostname + websocketPort + '/ws/notifications/';
 
-	let notificationSocket = {
-		socket: new WebSocket(notificationSocketUrl),
-		url: notificationSocketUrl,
-		shouldClose: false
-	};
+    let notificationSocket = {
+        socket: new WebSocket(notificationSocketUrl),
+        url: notificationSocketUrl,
+        shouldClose: false
+    };
 
-	// Update the notification count
-	notificationSocket.socket.onmessage = function(e) {
-		renderHeader();
-	};
+    // Update the notification count
+    notificationSocket.socket.onmessage = function(e) {
+        renderHeader();
+    };
 
-	
-	// Close the socket
-	notificationSocket.socket.onclose = function(e) {
-		if (!this.shouldClose) {
-			notificationSocket.socket = new WebSocket(notificationSocket.url);
-		}
-	};
+    // Close the socket
+    notificationSocket.socket.onclose = function(e) {
+        if (!this.shouldClose) {
+            // Reconnect if the socket is unexpectedly closed
+            notificationSocket.socket = new WebSocket(notificationSocket.url);
+        }
+    };
 
-
-	// Close the socket when the user leaves the page
-	window.onbeforeunload = function() {
-		if (notificationSocket !== null) {
-			notificationSocket.shouldClose = true;
-			notificationSocket.socket.close();
-		}
-	}
+    // Close the socket when the user leaves the page
+    window.onbeforeunload = function() {
+        if (notificationSocket !== null) {
+            notificationSocket.shouldClose = true;
+            notificationSocket.socket.close();
+        }
+    };
 });
