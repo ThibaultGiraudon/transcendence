c6ebb187333e2c5b3b9e2862d304c07f227bef5b
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-02-13 14:52:01 +0100

    change send functions

--- django/mainApp/consumers/gameConsumerUtils/senders/sendInitPaddlePosition.py
+++ django/mainApp/consumers/gameConsumerUtils/senders/sendInitPaddlePosition.py
@@ -1,20 +1,11 @@
-import json
-
 async def sendInitPaddlePosition(consumer, gameSettings):
 	for paddle in gameSettings.paddles:
 		if (paddle.isAlive == True):
-			if (gameSettings.isLocalGame == False):
-				await consumer.channel_layer.group_send(
-					f'game_{consumer.game_id}',
-					{
-						'type': 'init_paddle_position',
-						'position': paddle.position,
-						'id': paddle.id,
-					}
-				)
-			else:
-				await consumer.send(json.dumps({
+			await consumer.channel_layer.group_send(
+				f'game_{consumer.game_id}',
+				{
 					'type': 'init_paddle_position',
 					'position': paddle.position,
 					'id': paddle.id,
-				}))
+				}
+			)
