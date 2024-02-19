daaea4cb830bfc168d608c88abe4e585c52f73a4
LeonPupier <leonpupier@gmail.com>
2024-02-19 10:19:50 +0100

    Add Timeout Logout & global improvements

--- django/mainProject/settings.py
+++ django/mainProject/settings.py
@@ -112,6 +112,7 @@ MIDDLEWARE = [
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'mainApp.middleware.SessionTimeoutMiddleware.SessionTimeoutMiddleware',
 ]
 
 ROOT_URLCONF = 'mainProject.urls'
