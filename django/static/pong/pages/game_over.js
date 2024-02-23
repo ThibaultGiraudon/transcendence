5b0e4bc8d9748a0b43a127921aeed1f99a62a1ae
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-02-23 12:19:32 +0100

    add gameOver position  and score to front-end

--- django/static/pong/pages/game_over.js
+++ django/static/pong/pages/game_over.js
@@ -4,10 +4,23 @@ function renderGameOverPage(gameID) {
 			fetchAPI('/api/get_game_over/' + gameID).then(data => {
 				if (data.success) {	
 					playerID = data.player_id;
+					score = data.score;
+					position = data.position;
+					if (position == 1) {
+						position = '1st';
+					} else if (position == 2) {
+						position = '2nd';
+					} else if (position == 3) {
+						position = '3rd';
+					} else {
+						position = position + 'th';
+					}
 
 					let html = `
 						<h1>Game Over (id = ${gameID})</h1>
-						<p>Winner: ${playerID}</p>
+						<p>Player ID: ${playerID}</p>
+						<p>Score: ${score}</p>
+						<p>You finished ${position}</p>
 					`;
 					document.getElementById('app').innerHTML = html;
 				}
