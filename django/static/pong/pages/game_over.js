d1bdc58d9b55a751c4487ca0bfe793c15032cb9b
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-02-23 11:58:15 +0100

    add simple gameOver page

--- django/static/pong/pages/game_over.js
+++ django/static/pong/pages/game_over.js
@@ -1,15 +1,17 @@
 function renderGameOverPage(gameID) {
 	fetchAPI('/api/isAuthenticated').then(data => {
 		if (data.isAuthenticated) {
-			// fetchAPI('/api/get_game_info').then(data => {
-			// })
-			document.getElementById('app').innerHTML = `
-				<h1>Game Over</h1>
-			`;
+			fetchAPI('/api/get_game_over/' + gameID).then(data => {
+				if (data.success) {	
+					playerID = data.player_id;
 
-
-
-
+					let html = `
+						<h1>Game Over (id = ${gameID})</h1>
+						<p>Winner: ${playerID}</p>
+					`;
+					document.getElementById('app').innerHTML = html;
+				}
+			});
 		// If the user is not connected
 		} else {
 			router.navigate('/sign_in/');
